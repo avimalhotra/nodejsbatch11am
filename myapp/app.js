146d81ee54cb8cf33f49dc5e27480598a9c3ca41
Avinash Malhotra <avimalhotra505@gmail.com>
2020-06-10 12:19:40 +0530

    files uploaded

---
+++ myapp/app.js
@@ -0,0 +1,17 @@
+//console.log(2+7);
+//console.error("error found")
+//console.log("hi %s, how are you?","avi");  
+//console.log("x is %d and y is  %d.",2,3);      
+//console.log("This is %cred","color:red");
+//console.log("File Name: ", __filename);
+//console.log("Dir Name: ",__dirname);
+
+require('./test');          // module bind in app.js
+
+var x=3;
+global.name="node";
+
+//console.log(console==global.console);
+//console.log(name==global.name );
+
+console.log( user, id, x, name);
---
+++ myapp/test.js
@@ -0,0 +1,6 @@
+global.user="avi";
+global.id=212;
+
+const pass=123456;
+
+//console.log(user, id, pass);
