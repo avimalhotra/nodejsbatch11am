26e16dec85f08b5d458de30070a6e73d1a5d9b7d
Avinash Malhotra <avimalhotra505@gmail.com>
2020-06-19 12:00:59 +0530

    Uploaded FS

---
+++ src/fs.js
@@ -0,0 +1,51 @@
+const fs=require('fs');
+var data="\nhello user";
+
+
+//let file=fs.readFileSync(('src/file.txt'),{encoding:'utf-8'});
+
+//console.log(file);
+//console.log(file.toString());
+
+// fs.readFile('src/file.txt',{encoding:'utf-8'},(err,data)=>{
+//     if(err){
+//         console.error("error found")
+//     }
+//     else{
+//         console.log(data.toString());
+//     }
+// });
+
+
+
+// fs.writeFile('src/files/file1.txt',data,(err)=>{
+//     if(err){
+//         console.error("error found");
+//     }
+//     else{
+//         console.log("written in file");
+//     }
+// });
+
+// fs.appendFile('src/files/file4.txt','utf8',data,(err)=>{
+//     if(err){
+//         console.error("error found");
+//     }
+//     else{
+//         console.log("written in file");
+//     }
+// });
+
+
+//fs.unlinkSync('src/files/file4.txt');
+// fs.unlink('src/files/file3.txt',(err)=>{
+//     if(err){
+//         console.error('file not found')
+//     }
+//     else{
+//         console.log("file deleted")
+//     }
+// })
+
+
+console.log('app running');
