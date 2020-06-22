93de565ed1f616d3d6d6b157360956e8ec9d1238
Avinash Malhotra <avimalhotra505@gmail.com>
2020-06-22 16:32:44 +0530

    upload path.js

---
+++ src/path.js
@@ -0,0 +1,15 @@
+const path=require('path');
+
+//console.log(path.normalize('src/img/../'))
+//console.log(path.basename("./src/img/pic.png",".png"))
+
+//console.log(path.dirname("src/pic/pic.png"))
+//console.log(path.extname("logo.jpg"))
+
+
+//console.log(path.resolve("src"));
+
+//console.log(path.join('/foo',"bar"))
+//console.log(path.resolve(__dirname,'test/testapp'));
+
+console.log(path.resolve(__dirname,'views'))
